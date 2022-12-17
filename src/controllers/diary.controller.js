0587bb2db7c42ca7ebe7b02d6389fc885df66b60
Jinsol <jinsol0323@naver.com>
2022-12-17 18:19:17 +0900

    dairy_read_all_and_detail

--- src/controllers/diary.controller.js
+++ src/controllers/diary.controller.js
@@ -3,7 +3,7 @@ const DiaryService = require('../../src/services/diary.service');
 
 class DiaryController {
   diaryService = new DiaryService();
-  createDiary = async (req, res, next) => {
+  createDiary = async (req, res) => {
     const { title, image, content, weather } = req.body;
     const { userId } = res.locals.user;
     console.log(userId);
@@ -11,26 +11,18 @@ class DiaryController {
     return res.status(201).json({ message: '일기장 생성' });
   };
 
- //다이어리 전체 목록 조회
- findAllDiaries = async (req, res) => {
-    try {
-        const diaries = await this.diaryService.findAllDiaries();
-        return res.json({ diaries });
-    }   catch (err) {
-        console.err(err);
-        return res.status(405).json({ errorMessage: "일기장 전체 목록 조회 실패"});
-    }
-}; 
+ 
+  findAllDiaries = async (req, res) => {    
+    const diaries = await this.diaryService.findAllDiaries();
+    return res.status(201).json({ diaries });     
+  }; 
 
-//다이어리 상세 조회
-findDetailDiary = async (req, res) => {
-    try {
-        const { diaryId } = req.params;
-        const diary = await this.diaryService.findDetailDiary({ diaryId });
-        res.json({ diary });
-    }   catch (err) {
-        console.error(err);
-        res.status(406).jon({ errorMessage: "일기장 조회 실패"})
-    }
+
+  findDetailDiary = async (req, res) => {
+    const { diaryId } = req.params;
+    const diary = await this.diaryService.findDetailDiary({ diaryId });
+    return res.status(201).json({ diary });    
+  };
 }
-}module.exports = DiaryController;
+
+module.exports = DiaryController;
--- src/routers/diary.router.js
+++ src/routers/diary.router.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middlewares/auth');
+const DiaryController = require('../controllers/diary.controller');
+const diarycontroller = new DiaryController();
+
+router.post('/', authMiddleware, diarycontroller.createDiary);
+router.get('/', authMiddleware, diarycontroller.findDetailDiary);
+router.get('/', diarycontroller.findAllDiaries)
 
 module.exports = router;

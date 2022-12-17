3e7a5332dd0e8a683479fb3d54fc3c3a0ab37d0d
ludin-lee <alsdyd0331@naver.com>
2022-12-17 10:23:06 +0900

    login complete

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,14 +1,13 @@
-
 const express = require('express');
 const router = express.Router();
 const signupRouter = require('./signup.router');
-const authMiddleware = require("../middlewares/auth")
-
+const authMiddleware = require('../middlewares/auth');
+const loginRouter = require('../routers/');
 // const loginRouter = require('./login');
 // const diaryRouter = require("")
 
 router.use('/signup/', signupRouter);
-router.use("/login", loginRouter);
+router.use('/login', loginRouter);
 
 // router.use("/diary", diaryRouter);
 

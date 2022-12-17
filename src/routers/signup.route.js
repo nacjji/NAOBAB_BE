663cd2020bc3c1534aa604e43634e5ed5e18dc4d
Jiwon NA <jiwonna@Jiwonui-MacBookPro.local>
2022-12-17 10:16:42 +0900

    signup, login, authmiddleware before merge

--- src/routers/signup.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const SignupController = require('../controllers/signup.controller');
-const signupController = new SignupController();
-
-router.post('/', signupController.signupUser);
-
-module.exports = router;
--- src/routers/signup.router.js
+++ src/routers/signup.router.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const SignupController = require('../controllers/signup.controller');
 const router = express.Router();
+const SignupController = require('../controllers/signup.controller');
 const signupController = new SignupController();
 
 router.post('/', signupController.signupUser);

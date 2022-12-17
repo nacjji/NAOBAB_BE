5ad9dc0cd5c35b83a68c4396ac69e1440ab081ed
ludin-lee <alsdyd0331@naver.com>
2022-12-17 10:31:07 +0900

    login라우터에  미들웨어 제거

--- src/routers/login.route.js
+++ src/routers/login.route.js
@@ -1,11 +1,8 @@
 const { Router } = require('express');
 const LoginController = require('../controllers/login.controller');
-const authMiddleware = require('../middlewares/auth');
-
 const router = Router();
-
 const loginController = new LoginController();
 
-router.post('/', authMiddleware, loginController);
+router.post('/', loginController);
 
 module.exports = router;

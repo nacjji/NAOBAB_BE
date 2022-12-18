221133619ce9368f8286bdd739396c8aee2bb088
ludin-lee <alsdyd0331@naver.com>
2022-12-18 15:08:36 +0900

    다이어리 업데이트 및 삭제 구현

--- src/routers/diary.router.js
+++ src/routers/diary.router.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middlewares/auth');
 const DiaryController = require('../controllers/diary.controller');
+const { route } = require('./login.router');
 const diarycontroller = new DiaryController();
 
 router.post('/', authMiddleware, diarycontroller.createDiary);
 router.get('/:diaryId', authMiddleware, diarycontroller.findDetailDiary);
 router.get('/', diarycontroller.findAllDiaries);
-
+router.put('/:diaryId', authMiddleware, diarycontroller.updateDiary);
+router.delete('/:diaryId', authMiddleware, diarycontroller.deleteDiary);
 module.exports = router;
--- src/services/diary.service.js
+++ src/services/diary.service.js
@@ -1,8 +1,9 @@
 const DiaryRepository = require('../../src/repositories/diary.repository');
+const { Diary } = require('../../models');
 
 class DiaryService {
   constructor() {
-    this.diaryRepository = new DiaryRepository();
+    this.diaryRepository = new DiaryRepository(Diary);
   }
   diaryRepository = new DiaryRepository();
   createDiary = async (userId, title, image, content, weather) =>
@@ -21,9 +22,27 @@ class DiaryService {
   };
 
   //다이어리 상세 조회
-  findDetailDiary = async ({ diaryId }) => {
-    const diary = await this.diaryRepository.findDetailDiary({ diaryId });
+  findDetailDiary = async (diaryId) => {
+    const diary = await this.diaryRepository.findDetailDiary(diaryId);
     return diary;
+  };
+
+  //다이어리 업데이트
+  updateDiary = async (diaryId, title, image, content, weather) => {
+    const diaryInfo = await this.diaryRepository.findDetailDiary(diaryId);
+
+    return await this.diaryRepository.updateDiary(
+      diaryId,
+      title,
+      image,
+      content,
+      weather,
+    );
+  };
+
+  //다이어리 삭제
+  deleteDiary = async (diaryId) => {
+    return await this.diaryRepository.deleteDiary(diaryId);
   };
 }
 

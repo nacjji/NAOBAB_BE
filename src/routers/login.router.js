ae470a913c0257366f0b457a16f6f7968fbe8cfe
ludin-lee <alsdyd0331@naver.com>
2022-12-18 13:17:20 +0900

    Update login.router.js

--- src/routers/login.router.js
+++ src/routers/login.router.js
@@ -1,13 +1,10 @@
 const { Router } = require('express');
+const passport = require('passport');
 const LoginController = require('../controllers/login.controller');
 const router = Router();
 const loginController = new LoginController();
 
-
-
-
 router.post('/', loginController.login);
-
-
+router.get('/kakao', passport.authenticate('kakao'));
 
 module.exports = router;

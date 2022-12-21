2386c1a41a7817a8deee3b50ff4f48e25106e310
Jiwon NA <jiwonna@Jiwonui-MacBookPro.local>
2022-12-22 01:13:02 +0900

    회원 기본 이미지 설정

--- models/users.js
+++ models/users.js
@@ -32,6 +32,8 @@ module.exports = (sequelize, DataTypes) => {
       profileImg: {
         type: DataTypes.STRING,
         allowNull: true,
+        defaultValue:
+          'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
       },
       selfIntro: {
         type: DataTypes.STRING,
